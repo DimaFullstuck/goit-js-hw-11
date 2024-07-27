0461ac42424147d9927cdebfa9b1c226a24853cd
Dima <dima1999ostapenko@gmail.com>
2024-07-27 15:28:43 +0300

    add hm11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+export default function searchImagesByQuery(query) {
+  const URL = 'https://pixabay.com/api/';
+  const API_KEY = '45142135-0c3dcc8f867495c741718f1c0';
+
+  return fetch(
+    `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+  //     .catch((error) => {
+  //     iziToast.error({
+  //         position: "topRight",
+  //         message: `${error}`,
+  //     });
+  // });
+}
